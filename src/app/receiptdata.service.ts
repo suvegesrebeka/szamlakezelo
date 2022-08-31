25c1216345c7109722f0becf140422263c019830
srebii <suvegesrebeka@gmail.com>
2022-08-31 11:56:14 +0200

    service megcsinalasa

--- src/app/receiptdata.service.ts
+++ src/app/receiptdata.service.ts
@@ -1,45 +1,29 @@
-import { Injectable } from '@angular/core';
-import { Subscription, Observable, Subject } from 'rxjs';
+import { EventEmitter, Injectable, OnInit } from '@angular/core';
+import { Subscription, Observable, BehaviorSubject, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReceiptdataService {
 
-  constructor() { }
 
-  receipts = [
-    { cusName: "Barka", buyDate: "2019-01-23", realDate: "2020-12-21", product: "macska", cusComment: "Nem tetszik", prodPrice: 233423 }
-  ]
+  private sourceBehav = new BehaviorSubject<any>(null);
+  public behaveiorSubject$ = this.sourceBehav.asObservable();
 
 
-  //2. try with Observable, subject, subscription
-  // private subject = new Subject<any>();
-
-  // sendData2(name: string, date: string, date2: string, item: string, comment: string, price: number) {
-  //   this.subject.next({ cusName: name, buyDate: date, realDate: date2, product: item, cusComment: comment, prodPrice: price });
-  // }
-
-  // getData(): Observable<any> {
-  //   return this.subject.asObservable();
-  // }
-
-    // sendData(name:string, date:string,date2:string,item:string,comment:string,price:number){
-  //   this.subject.next({})
-  // }
-   // addNewReceipt(name: string, date: string, date2: string, item: string, comment: string, price: number) {
-  //   this.receipts.push({ cusName: name, buyDate: date, realDate: date2, product: item, cusComment: comment, prodPrice: price });
-  // }
-
-  //3.try
-    sendMessage = new Subject<any>();
-
-   communicateMessage(name: string, date: string, date2: string, item: string, comment: string, price: number){
-      this.sendMessage.next({ cusName: name, buyDate: date, realDate: date2, product: item, cusComment: comment, prodPrice: price })
-   }
-   getData(name: string, date: string, date2: string, item: string, comment: string, price: number){
-    this.receipts.push({ cusName: name, buyDate: date, realDate: date2, product: item, cusComment: comment, prodPrice: price });
-    console.log(this.receipts+"ez a szörviz")
+  constructor() {
   }
+
+  receipts: any = []
+
+  showReceipt(value: any) {
+    for (const [key, val] of Object.entries(value)) {
+      console.log(`${key}: ${val}`);
+    }
+    this.sourceBehav.next(value)
+
+  }
+
+
 
 }
