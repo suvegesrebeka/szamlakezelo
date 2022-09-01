add6ef223b55a3cfd8a6671f8f78e7f749c4ef1b
srebii <suvegesrebeka@gmail.com>
2022-09-01 16:19:18 +0200

    responsive done

--- src/app/components/check/check.component.ts
+++ src/app/components/check/check.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,OnDestroy } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { DatePipe } from '@angular/common'
 import { Router } from '@angular/router';
 import { ReceiptdataService } from 'src/app/receiptdata.service';
-import { Subscription, Observable, Subject } from 'rxjs';
+import { Subscription, Observable, Subject,of } from 'rxjs';
+import { UniquedataService} from 'src/app/uniquedata.service';
 
 
 @Component({
@@ -14,24 +15,30 @@ import { Subscription, Observable, Subject } from 'rxjs';
 })
 export class CheckComponent {
 
- 
-  constructor(private router: Router, private receiptdataService: ReceiptdataService) {
-    this.subscription();
-   }
+
+  receiptObservable$: Observable<any[]> = of([]);
+
+  constructor(
+    private router: Router,
+    private receiptdataService: ReceiptdataService,
+    private uniquedataService : UniquedataService
+  ) {}
+
   ngOnInit(): void {
-    console.log("check oninit");
-
-   
+    this.receiptObservable$ = this.receiptdataService.behaveiorSubject$;
   }
+onClick(...value:any){
+ console.log(value)
+ var receiptVal={
+  cusName:value[0],
+  buyDate: value[1],
+  realDate: value[2],
+  product: value[3],
+  cusComment: value[4],
+  prodPrice: value[5],
+ }
+ 
+ this.uniquedataService.addDetails(receiptVal)
+}
 
-  receipts:any[]=[];
-  subscription(){
-    this.receiptdataService.behaveiorSubject$.subscribe((res:any)=>{
-      for (const [key, val] of Object.entries(res)) {
-        console.log(`${key}: ${val} + 2. respo`);
-      }
-      this.receipts.push(res)
-      console.log(this.receipts +" thats the array")
-    })
-  }
 }
